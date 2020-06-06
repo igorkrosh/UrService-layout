b199248439a065828fe20b5f25ca61bbba7216f3
igor.krosh <igor.krosh@gmail.com>
2020-06-06 17:13:06 +0300

    Main slider - Десктоп

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        dots: true,
+        nav: true,
+        dotsContainer: '.carousel-nav-dots',
+        navContainer: '.carousel-nav-arrows',
+        navText:["<img src='assets/images/icons/slider-arrow.png'>","<img src='assets/images/icons/slider-arrow.png'>"],
+    });
+  });
