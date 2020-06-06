d69c8e0be0cecdfb5a06273b16a5aebdfce240e2
igor.krosh <igor.krosh@gmail.com>
2020-06-07 02:44:26 +0300

    Наши клиенты & Наша специализация - Десктоп

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".carousel .owl-carousel").owlCarousel({
         items: 1,
         dots: true,
         nav: true,
         dotsContainer: '.carousel-nav-dots',
         navContainer: '.carousel-nav-arrows',
         navText:["<img src='assets/images/icons/slider-arrow.png'>","<img src='assets/images/icons/slider-arrow.png'>"],
+    });
+    $(".our-clients .owl-carousel").owlCarousel({
+        items: 6,
+        margin: 50,
+        autoWidth: true
+        
     });
   });
